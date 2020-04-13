c03ea31d096bbeb28f13e8f78a89cca3e5899b11
kazuomatz <g0129matsu>
2020-04-13 09:59:54 +0900

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :admin do
     resources :login_histories
   end
-  namespace :admin do
-    resources :companies
-  end
-  namespace :admin do
-    resources :animals
-  end
-  namespace :admin do
-    resources :locations
-  end
-
-  namespace :admin do
-    resources :events
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
