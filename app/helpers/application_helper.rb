32e7118dd00120a110ea76364d817dd2f6961381
kazuomatz <g0129matsu>
2020-06-05 09:39:55 +0900

    generator エラー時のrenderのform指定修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -139,4 +139,27 @@ module ApplicationHelper
     raw html
   end
 
+  def print_flash_alert(object, alert)
+    html = '<ul class="flash-alert">'
+    if alert.respond_to?('each')
+      alert.each do |key, values|
+        if values.respond_to?('each')
+          values.each do |value|
+            if object.respond_to?('form_column') && object.form_column(key).present?
+              html += '<li>' + object.form_column(key)[:label] + value.to_s + '</li>'
+            else
+              html += '<li>' + key.to_s + value.to_s + '</li>'
+            end
+          end
+        else
+          html += '<li>' + key.to_s +  values.to_s + '</li>'
+        end
+      end
+    else
+      html += '<li>' + key.to_s + '</li>'
+    end
+    html += '</ul>'
+    raw html
+  end
+
 end
